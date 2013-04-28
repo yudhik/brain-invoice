f3fbdfebdb6085295663c6e26ac6802ce1cd95a3
yudhik <brainmaster716@gmail.com>
2013-04-28 12:55:55 +0700

    implement brand account using mapped super class of accountKeys

--- brain-invoice-model/src/main/java/com/brainmaster/apps/invoicing/model/id/BrandAccountKeys.java
+++ brain-invoice-model/src/main/java/com/brainmaster/apps/invoicing/model/id/BrandAccountKeys.java
@@ -4,37 +4,22 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
 
 import com.brainmaster.apps.invoicing.model.Account;
 
 @Embeddable
-public class BrandAccountKeys implements Serializable {
+public class BrandAccountKeys extends AccountKeys implements Serializable {
 
 	private static final long serialVersionUID = -5852654524371945799L;
-	
-	@ManyToOne(targetEntity = Account.class)
-	@JoinColumn(name = "account_id")
-	private Account account;
-	
+
 	@Column(name = "brand_name")
 	private String brandName;
-	
-	public BrandAccountKeys() {
-	}
 
 	public BrandAccountKeys(Account account, String brandName) {
-		this.account = account;
+		super(account);
 		this.brandName = brandName;
-	}
-
-	public Account getAccount() {
-		return account;
-	}
-
-	public void setAccount(Account account) {
-		this.account = account;
 	}
 
 	public String getBrandName() {
@@ -44,12 +29,14 @@ public class BrandAccountKeys implements Serializable {
 	public void setBrandName(String brandName) {
 		this.brandName = brandName;
 	}
+	
+	//TODO: implement hashcode, equals and toString into a proper implementation
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((account == null) ? 0 : account.hashCode());
+		result = super.hashCode();
 		result = prime * result
 				+ ((brandName == null) ? 0 : brandName.hashCode());
 		return result;
@@ -64,25 +51,14 @@ public class BrandAccountKeys implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		BrandAccountKeys other = (BrandAccountKeys) obj;
-		if (account == null) {
-			if (other.account != null)
-				return false;
-		} else if (!account.equals(other.account))
-			return false;
-		if (brandName == null) {
-			if (other.brandName != null)
-				return false;
-		} else if (!brandName.equals(other.brandName))
-			return false;
-		return true;
+		return new EqualsBuilder().appendSuper(super.equals(other))
+				.append(brandName, other.getBrandName()).isEquals();
 	}
 
 	@Override
 	public String toString() {
-		return "BrandAccountKeys [account=" + account.getAccountId() + ", brandName="
-				+ brandName + "]";
+		return "BrandAccountKeys [account=" + getAccount().getAccountId()
+				+ ", brandName=" + brandName + "]";
 	}
-	
-	
-	
+
 }
